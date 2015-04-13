69d507c13b4cb834c9fea96d04c033cce6b9f78e
peterxjang <peter.x.jang@gmail.com>
2015-04-13 08:47:15 -0500

    switched audio preview to html5 element

--- client/templates/songinfo.js
+++ client/templates/songinfo.js
@@ -33,6 +33,59 @@ Template.songinfo.helpers({
   }
 });
 
+Template.songinfo.loadLyrics = function(artist, title) {
+  var url =  'http://api.lyricsnmusic.com/songs?' +
+             'api_key=74fb7d12d34928ad62f09d18cd0842&' +
+             'artist=' + artist + '&track=' + title;
+  $.ajax({
+    url: url,
+    jsonp: 'callback',
+    dataType: 'jsonp',
+    data: {format: 'json'},
+    success: function(response) {
+      if (response.data.length > 0) {
+        $('.song-lyrics-results').html(response.data[0].snippet.replace(/\n/g, '<br>'));
+        $('.song-lyrics-results').append(
+          '<div><a href=' + response.data[0].url +
+          ' target="_blank">View complete lyrics</a><div>'
+        );
+      } else {
+        $('.song-lyrics-results').html('No lyrics found');
+      }
+    },
+    error: function() {
+      $('.song-lyrics-results').html('No lyrics found');
+    },
+    complete: function() {
+      $('.ui.loading.segment').removeClass('loading');
+    }
+  });
+};
+
+Template.songinfo.loadAudioClip = function(artist, title) {
+  var url = 'https://api.spotify.com/v1/search?' +
+            'query=artist:' + artist + '+track:' + title + '&' +
+            'limit=1&type=track';
+  // $('audio').src = url;
+  $.ajax({
+    url: url,
+    success: function(response) {
+      var items = response.tracks.items;
+      if (items.length > 0) {
+      //   if (Template.songinfo.audioObject.src != items[0].preview_url) {
+      //     Template.songinfo.audioObject = new Audio(items[0].preview_url);
+      //   }
+      //   Template.songinfo.audioObject.play();
+      //   $(that).html('Pause');
+        $('.song-preview').html('<audio src="' + items[0].preview_url + '" controls></audio>');
+      } else {
+        $('.song-preview').html('Preview not available');
+      }
+    }
+  });
+  
+};
+
 Template.songinfo.rendered = function() {
   Template.songinfo.audioObject = new Audio();
 
@@ -42,32 +95,8 @@ Template.songinfo.rendered = function() {
       if (info) {
         var artist = info.artist.replace(/ /g, '%20');
         var title = info.title.replace(/\(([^)]+)\)/g, '').replace(/ /g, '%20');
-        var url =  'http://api.lyricsnmusic.com/songs?' +
-                   'api_key=74fb7d12d34928ad62f09d18cd0842&' +
-                   'artist=' + artist + '&track=' + title;
-        $.ajax({
-          url: url,
-          jsonp: 'callback',
-          dataType: 'jsonp',
-          data: {format: 'json'},
-          success: function(response) {
-            if (response.data.length > 0) {
-              $('.song-lyrics-results').html(response.data[0].snippet.replace(/\n/g, '<br>'));
-              $('.song-lyrics-results').append(
-                '<div><a href=' + response.data[0].url +
-                ' target="_blank">View complete lyrics</a><div>'
-              );
-            } else {
-              $('.song-lyrics-results').html('No lyrics found');
-            }
-          },
-          error: function() {
-            $('.song-lyrics-results').html('No lyrics found');
-          },
-          complete: function() {
-            $('.ui.loading.segment').removeClass('loading');
-          }
-        });
+        Template.songinfo.loadLyrics(artist, title);
+        Template.songinfo.loadAudioClip(artist, title);
       } else {
         $('.song-lyrics-results').html('No lyrics found');
       }
