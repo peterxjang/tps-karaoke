508ce42cfda8f4095f9c61a45d2139de5d399921
peterxjang <peter.x.jang@gmail.com>
2015-04-25 16:53:13 -0500

    added moment.js for readable time info per message

--- client/templates/admin.js
+++ client/templates/admin.js
@@ -1,6 +1,13 @@
 Template.admin.helpers({
   messages: function() {
-    return Messages.find({}, {limit: 10, sort: {createdAt: -1}});
+    var messages = Messages.find({}, {limit: 10, sort: {createdAt: -1}});
+    return messages.map(function(message) {
+      return {
+        username: message.username,
+        text: message.text,
+        createdAt: moment(message.createdAt).fromNow()
+      };
+    });
   }
 //   songs: function() {
 //     return Songs.find({}, {limit: 10, skip: 0, sort: {artist: 1}});
